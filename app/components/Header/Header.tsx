84fcc75ed82520e38f877d62b96e056e2d2a2f4b
Pavel <allpavel@yandex.ru>
2023-11-27 16:18:05 +0300

    add fonts and small changes

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -3,6 +3,9 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header>
+      <a href="/" className="font-font-ephesis">
+        Pavel Alloyarov
+      </a>
       <nav>
         <Link href={"/about"}>О себе</Link>
         <Link href={"/projects"}>Проекты</Link>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,13 @@
 import Header from "./components/Header/Header";
+import { Ephesis } from "next/font/google";
 import "./globals.css";
+
+const ephesis = Ephesis({
+  subsets: ["latin"],
+  weight: ["400"],
+  display: "swap",
+  variable: "--ephesis",
+});
 
 export default function RootLayout({
   children,
@@ -7,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={ephesis.variable}>
       <body>
         <Header />
         {children}
