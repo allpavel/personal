e02c32be9decff0098a8a3d4591140e22a4d41f2
Pavel Alloyarov <platprincipal.adv@gmail.com>
2023-12-18 20:02:59 +0300

    fix skip link and links in a navbar

--- app/components/NavigationMenu/NavigationMenu.tsx
+++ app/components/NavigationMenu/NavigationMenu.tsx
@@ -5,7 +5,7 @@ export function NavigationMenu({ isOpen }: { isOpen: boolean }) {
     <nav
       role="navigation"
       aria-label="Main menu"
-      className={`absolute left-0 right-0 top-9 flex flex-col gap-4 bg-zinc-50 transition-visible duration-300 motion-reduce:transition-none motion-reduce:hover:transform-none sm:visible sm:static sm:flex-row sm:opacity-100 first-letter:${
+      className={`absolute left-0 right-0 top-9 flex flex-col gap-4 bg-zinc-50 transition-visible duration-300 motion-reduce:transition-none motion-reduce:hover:transform-none sm:visible sm:static sm:flex-row sm:opacity-100 ${
         isOpen ? "visible opacity-100 " : "invisible opacity-0"
       }`}
     >
--- app/components/SkipLink/SkipLink.tsx
+++ app/components/SkipLink/SkipLink.tsx
@@ -4,7 +4,7 @@ export function SkipLink() {
   return (
     <Link
       href={"#maincontent"}
-      className="absolute left-1/2 -translate-x-1/2 -translate-y-[calc(100%+1rem)] bg-slate-500 text-center text-zinc-50 transition-transform focus:translate-y-0"
+      className="absolute left-1/2 -translate-x-1/2 -translate-y-[calc(100%+1.3rem)] bg-slate-500 text-center text-zinc-50 transition-transform focus:translate-y-0"
     >
       Skip to the main content
     </Link>
