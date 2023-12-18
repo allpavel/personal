b9f67ec85644f1c794146ddd7cfbf7436bee854a
Pavel Alloyarov <platprincipal.adv@gmail.com>
2023-12-18 16:34:31 +0300

    push footer to the bottom

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,3 +1,11 @@
+import { Logo } from "../Logo/Logo";
+
 export default function Footer() {
-  return <footer>All rights reserved &copy; Pavel Alloyarov 2023</footer>;
+  return (
+    <footer>
+      <Logo />
+      <div>All rights reserved &copy; Pavel Alloyarov 2023</div>
+      <div>Social media</div>
+    </footer>
+  );
 }
